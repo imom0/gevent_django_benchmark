db3be6c173762d86aaca95b7a9aa269caa249d35
yangyang <yangyang05@autonavi.com>
2014-09-03 23:19:47 +0800

    增加gevent+sync配置

--- run_tests.sh
+++ run_tests.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
 
+function get_unused_port {
+  export PORT=`netstat -atn | perl -0777 -ne '@ports = /tcp.*?\:(\d+)\s+/imsg ; for $port (32768..61000) {if(!grep(/^$port$/, @ports)) { print $port; last } }'`
+}
 function run_test {
   sleep 2
   wrk -t20 -c500 -d10s "http://127.0.0.1:$PORT/" -s bm.lua | grep "Requests/sec"
 }
 
-
-export PORT=7891
+get_unused_port
 echo 'gunicorn+gvent+pymysql'
 PYMYSQL=1 gunicorn -c conf/g_gevent_conf.py myproj.wsgi:application
 run_test
 kill -INT `cat g_gevent.pid`
 
-export PORT=7892
-sleep 3
+get_unused_port
 echo 'gunicorn+gvent+mysql-python'
 PYMYSQL=0 gunicorn -c conf/g_gevent_conf.py myproj.wsgi:application
 run_test
 kill -INT `cat g_gevent.pid`
+
+get_unused_port
+echo 'gunicorn+sync+pymysql'
+PYMYSQL=1 gunicorn -c conf/g_sync_conf.py myproj.wsgi:application
+run_test
+kill -INT `cat g_sync.pid`
+
+get_unused_port
+echo 'gunicorn+sync+mysql-python'
+PYMYSQL=0 gunicorn -c conf/g_sync_conf.py myproj.wsgi:application
+run_test
+kill -INT `cat g_sync.pid`
