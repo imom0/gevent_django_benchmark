49988c2414f412e69f80b2d8fb11b3ac550049fd
yangyang <yangyang05@autonavi.com>
2014-09-04 00:29:08 +0800

    加入uwsgi测试

--- myproj/gwsgi.py
+++ myproj/gwsgi.py
@@ -6,6 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+from gevent import monkey
+monkey.patch_all()
 
 import os
 if os.environ.get('PYMYSQL') == '1':
--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,8 @@
 
 
 function get_unused_port {
-  export PORT=`netstat -atn | perl -0777 -ne '@ports = /tcp.*?\:(\d+)\s+/imsg ; for $port (32768..61000) {if(!grep(/^$port$/, @ports)) { print $port; last } }'`
+  # ref: http://www.commandlinefu.com/commands/view/7299/find-an-unused-unprivileged-tcp-port
+  export PORT=`netstat -atn | perl -0777 -ne '@ports = /tcp.*?\:(\d+)\s+/imsg ; for $port (50768..61000) {if(!grep(/^$port$/, @ports)) { print $port; last } }'`
 }
 function run_test {
   sleep 2
@@ -32,3 +33,24 @@ echo 'gunicorn+sync+mysql-python'
 PYMYSQL=0 gunicorn -c conf/g_sync_conf.py myproj.wsgi:application
 run_test
 kill -INT `cat g_sync.pid`
+
+export PORT=7897
+echo 'uwsgi+sync+pymysql'
+uwsgi --env PYMYSQL=1 -s /tmp/uwsgiaaa.sock --wsgi-file myproj/wsgi.py --master --pidfile=uwsgi.pid --processes 9 --daemonize uwsgi.log --listen 1024
+run_test
+kill -INT `cat uwsgi.pid`
+
+echo 'uwsgi+sync+mysql-python'
+uwsgi --env PYMYSQL=0 -s /tmp/uwsgiaaa.sock --wsgi-file myproj/wsgi.py --master --pidfile=uwsgi.pid --processes 9 --daemonize uwsgi.log --listen 1024
+run_test
+kill -INT `cat uwsgi.pid`
+
+echo 'uwsgi+gevent+pymysql'
+uwsgi --env PYMYSQL=1 --gevent 100 -s /tmp/uwsgiaaa.sock --wsgi-file myproj/gwsgi.py --master --pidfile=uwsgi.pid --processes 9 --daemonize uwsgi.log --listen 1024
+run_test
+kill -INT `cat uwsgi.pid`
+
+echo 'uwsgi+gevent+mysql-python'
+uwsgi --env PYMYSQL=0 --gevent 100 -s /tmp/uwsgiaaa.sock --wsgi-file myproj/gwsgi.py --master --pidfile=uwsgi.pid --processes 9 --daemonize uwsgi.log --listen 1024
+run_test
+kill -INT `cat uwsgi.pid`
